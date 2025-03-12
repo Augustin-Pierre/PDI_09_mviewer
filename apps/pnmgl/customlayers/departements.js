1db3e129633f153777bbb94bdebff105a9b734b4
Timoonz <timotheeeheraud@gmail.com>
2025-03-12 14:48:09 +0100

    Update departements.js

--- apps/pnmgl/customlayers/departements.js
+++ apps/pnmgl/customlayers/departements.js
@@ -1,7 +1,7 @@
 const layer = new ol.layer.Tile({
     source: new ol.source.TileWMS({
         url: 'http://localhost:8080/geoserver/wms',
-        params: {'LAYERS': 'dimodere:departement', 'TILED': true},
+        params: {'LAYERS': 'pnmgl:departement', 'TILED': true},
     })
 })
 new CustomLayer('departement', layer)
